8e66bf3076a83192b78bef6c2c5ea7081b31d5e8
zwd <17326195691@163.com>
2023-09-07 15:08:52 +0800

    init

---
+++ pages/index/footer-box.vue
@@ -0,0 +1,75 @@
+<template>
+  <view class="counselorWrapper">
+    <u-row gutter="14" customStyle="margin-bottom: 30px">
+      <u-col span="2">
+        <u-avatar :src="src"></u-avatar>
+      </u-col>
+      <u-col span="10">
+        <view class="nameWrapper">
+          <text>任淑丽</text>
+          <text class="address">东岸观邸-云尚丹霞</text>
+        </view>
+        <view class="region">浦东新区·川沙 | 均价64000元/m²</view>
+      </u-col>
+    </u-row>
+    <u-row gutter="24">
+      <u-col span="6">
+        <view class="button btn1">加我微信</view>
+      </u-col>
+      <u-col span="6">
+        <view class="button btn2">电话咨询</view>
+      </u-col>
+    </u-row>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "FooterBox",
+  data() {
+    return {
+      src: "http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2119_s.jpg",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+.counselorWrapper {
+  padding: 10px 14px 40px 14px;
+  box-sizing: border-box;
+
+  .nameWrapper {
+    .address {
+      font-size: 14px;
+      margin-left: 10px;
+      background-color: #fcbd71;
+      padding: 2px 4px;
+      border-radius: 20px;
+      color: #333;
+    }
+  }
+  .region {
+    padding-top: 10px;
+    color: #ccc;
+    font-size: 13px;
+  }
+
+  .button {
+    width: 100%;
+    height: 34px;
+    border: 1px solid #2979ff;
+    border-radius: 6px;
+    line-height: 34px;
+    text-align: center;
+  }
+  .btn1 {
+    color: #2979ff;
+  }
+  .btn2 {
+    background-color: #2979ff;
+    color: #fff;
+  }
+}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <view class="main">
+    <u-navbar title="楼盘详情">
+      <view slot="left">
+        <u-icon name="home" size="20"></u-icon>
+      </view>
+    </u-navbar>
+
+    <view class="footerBox">
+      <FooterBox />
+    </view>
+  </view>
+</template>
+
+<script>
+import FooterBox from "./footer-box.vue";
+export default {
+  components: { FooterBox },
+  data() {
+    return {
+      value6: 0,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+.main {
+  position: relative;
+
+  .footerBox {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 170px;
+    box-shadow: 0 0 2px 1px rgba(77, 77, 77, 0.2);
+  }
+}
+</style>
